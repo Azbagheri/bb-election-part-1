0e9d2e9e4a4fec92b48c333c20d74d0fcf6ced18
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-02 19:19:46 -0400

    Adding Bikini Bottom Election-Part1

--- election.js
+++ election.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
-  // Imagination!
+$.ajax({
+  url: 'https://bb-election-api.herokuapp.com/',
+  method: 'GET',
+  dataType: 'json'
+}).done(function(data){
+
+    $('<div>').appendTo('body');
+    $('div').addClass('candidate');
+    $('<ul>').attr('id', 'candidate_list').appendTo('.candidate');
+
+    var candids = data['candidates'];
+    debugger;
+    for(i = 0; i < candids.length; i++){
+      var vote = candids[i]['votes'].toString();
+      $('<li>').html('Name: ' + candids[i]['name'] + ', Votes:' + vote  ).appendTo('#candidate_list');
+    }
+});
 
 });
